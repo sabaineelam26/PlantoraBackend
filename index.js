bf4d0c9dd0af97edbc18eb602feb286848ffba14
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2026-09-16 13:05:31 +0530

    chore: modified file

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const { initializeDB } = require("./db/db.connect"); 
+const { initializeDB } = require("./db/db.connect");
+const express = require("express"); 
+const app = express(); 
+const cors = require("cors"); 
+require("dotenv").config();  
 
+
+app.use(cors()); 
+app.use(express.json()); 
 
 
 initializeDB(); 
+
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const Admin = require("../models/Admin");
+const User = require("../models/User");
 
 const protect = async (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -9,7 +9,7 @@ const protect = async (req, res, next) => {
   try {
     const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await Admin.findById(decoded.id).select("-password");
+    req.user = await User.findById(decoded.id).select("-password");
     next();
   } catch {
     res.status(401).json({ message: "Token invalid" });
